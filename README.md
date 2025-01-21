1994021ceeebe9ccb43149d0d1b5301eaba349e1
Doot <niconez04@icloud.com>
2025-01-21 17:02:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RightClickRedone
+ Mod aimed at rebalancing the right-click functionality by editing vanilla block cards. 
---
+++ RightClickRedone/RightClickRedone.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using RightClickRedone.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace RightClickRedone {
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class RCRedone : BaseUnityPlugin {
+
+        private const string ModId = "com.delta.rounds.RCRedone";
+        private const string ModName = "Right Click Redone";
+        public const string Version = "0.1.0";
+        public const string ModInitials = "RCR";
+        public static RCRedone instance {get;private set;}
+
+        void Awake() {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start() {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>();
+        }
+    }
+}
